b4c6db684efe08241f2ddfe06098bd0ae2a43b9e
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-21 03:53:47 +0000

    added cards

---
+++ src/Cards.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Cards.css'
+
+const Cards = (props) => {
+    return (
+        <div />
+        );
+};
+
+Cards.propTypes = {
+};
+
+export default Cards;
