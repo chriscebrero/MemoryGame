b8347e7959767cbeb146ac535cb7daedcd5da8a9
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-21 03:59:59 +0000

    added props

--- src/Cards.js
+++ src/Cards.js
@@ -3,12 +3,18 @@ import PropTypes from 'prop-types';
 import './Cards.css'
 
 const Cards = (props) => {
+    let style = {};
+    if (props.showing) {
+        style.backgroundColor = props.backgroundColor;
+    }
     return (
-        <div />
+        <div className="card-container" style={style} />
         );
 };
 
 Cards.propTypes = {
+    showing: PropTypes.bool.isRequired,
+    backgroundColor: PropTypes.string.isRequired
 };
 
 export default Cards;
